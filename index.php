1d26c6be61ca954cf4068f210b8ee4c5d4fb6062
David AQ <153019747@qq.com>
2012-07-18 16:24:54 +0800

    start copying....

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$_title='#16 Studio';
+
+?>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title><?php echo $_title;?></title>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+	<script type="text/javascript" src="jquery.js"></script>
+	<script type="text/javascript" src="style.js"></script>
+</head>
+<body>
+	<div class="header fixedWidth"><img src="images/logo.png" class="logo"/></div>
+	<div class="nav fixedWidth">
+		<span class="top"></span>
+		<a href="">首页</a>
+		<a href="">博客</a>
+		<a href="">项目</a>
+		<a href="">关于我们</a>
+		<a href="">联系我们</a>
+		<a href="">友情链接</a>
+		<span class="bottom"></span>
+	</div>
+</body>
+</html>
---
+++ style.js
@@ -0,0 +1,9 @@
+$(function(){
+	$('.header .logo,.nav a').hover(function(){
+		$(this).stop(true,true);
+		$(this).animate({opacity:0.6},300);
+	},function(){
+		$(this).animate({opacity:1},300);	
+	});
+	
+});
