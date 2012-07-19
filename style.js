bb18b3225425eb30af1f55d2df67e23bae952064
David AQ <153019747@qq.com>
2012-07-19 12:30:52 +0800

    thumb

--- style.js
+++ style.js
@@ -11,13 +11,34 @@ $(function(){
 		sliderShow(0);
 		var slideCount=$('.slideShow .board .item').length;
 		var currSlide=0;
+		var playing=false;
 		function play(){
 			sliderHide(currSlide);
 			currSlide=(currSlide+1)%slideCount;
 			sliderShow(currSlide);
 		}
+		function sliderShow(index){
+			playing=true;
+			setTimeout(function(){
+				playing=false;
+			},500);
+			var board=$('.slideShow .board .item:eq('+index+')');
+			board.css({left:70,opacity:0});
+			board.show();
+			board.animate({left:100,opacity:1},500);
+			$('.slideShow .thumb .item:eq('+index+') .frame').animate({marginTop:0},500);
+			$('.slideShow .screen .item:eq('+index+')').fadeIn(500);
+		}
+		function sliderHide(index){
+			var board=$('.slideShow .board .item:eq('+index+')');
+			board.animate({left:170,opacity:0},500,function(){$(this).hide();});
+			$('.slideShow .thumb .item:eq('+index+') .frame').animate({marginTop:10},500);
+			$('.slideShow .screen .item:eq('+index+')').fadeOut(500);
+		}
 		var playTick=setInterval(play,7000);
 		$('.slideShow .thumb .item .frame').click(function(){
+			if(playing)
+				return;
 			clearInterval(playTick);
 			playTick=setInterval(play,7000);
 			var clicked=$(this).find('img').attr('class');
@@ -29,17 +50,3 @@ $(function(){
 		});
 	}
 });
-function sliderShow(index){
-	var board=$('.slideShow .board .item:eq('+index+')');
-	board.css({left:70,opacity:0});
-	board.show();
-	board.stop(true,true).animate({left:100,opacity:1},500);
-	$('.slideShow .thumb .item:eq('+index+') .frame').stop(true,true).animate({marginTop:0},500);
-	$('.slideShow .screen .item:eq('+index+')').stop(true,true).fadeIn(500);
-}
-function sliderHide(index){
-	var board=$('.slideShow .board .item:eq('+index+')');
-	board.stop(true,true).animate({left:170,opacity:0},500,function(){$(this).hide();});
-	$('.slideShow .thumb .item:eq('+index+') .frame').stop(true,true).animate({marginTop:10},500);
-	$('.slideShow .screen .item:eq('+index+')').stop(true,true).fadeOut(500);
-}
